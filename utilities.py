01f5ea399977ddabed91615ef51b76fb950d0341
seilerar <seilerar@rose-hulman.edu>
2024-10-05 17:53:56 -0400

    Fix pylint warnings

--- utilities.py
+++ utilities.py
@@ -1,4 +1,7 @@
+"""Some useful utilities for the pyton scripts"""
+
 import subprocess
 
 def run_command(command):
-	return subprocess.check_output(command, shell=True).decode()
+    """Run a command in a forked child and return the stdout"""
+    return subprocess.check_output(command, shell=True).decode()
