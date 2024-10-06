30399a2eec1bee7619704041453d3bdf863a0fda
seilerar <seilerar@rose-hulman.edu>
2024-10-06 16:31:16 -0400

    Get the tests running for now

--- test_usermode.py
+++ test_usermode.py
@@ -7,13 +7,13 @@ def compile_i386():
         "make clean",
         "make all",
         "popd"])
-    if not os.path.isfile("usermode/helloworld"):
+    if not os.path.isfile("usermode/build/helloworld"):
         raise Exception(f"{compile_i386.__name__} failed: No output was produced")
 
-    if get_stdout(run_command("usermode/helloworld")) != "hello world\n":
+    if get_stdout(run_command("usermode/build/helloworld")) != "hello world\n":
         raise Exception(f"{compile_i386.__name__} failed: The output was not 'hello world'")
 
-    if get_stderr(run_command("ldd usermode/helloworld")).strip() != "not a dynamic executable":
+    if get_stderr(run_command("ldd usermode/build/helloworld")).strip() != "not a dynamic executable":
         raise Exception(f"{compile_i386.__name__} failed: The output was not statically linked")
     
     print("Task successful!")
