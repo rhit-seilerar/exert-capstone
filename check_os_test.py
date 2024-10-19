277f982bfc4aa7b3e4bb2d56ccc674b49030205c
rhit-morehask <morehask@rose-hulman.edu>
2024-10-19 17:04:36 -0400

    Added a demo file for detecting OS, hopefully this will allow us to have a program that works across multiple systems

---
+++ check_os_test.py
@@ -0,0 +1,8 @@
+import platform
+# since startup and attach to container use diff commands depending on OS, we decided to separate those commands into a new file
+# then we have the exert filter out which commands to use based on the system detected. This is a demonstration on how to get the system name and work with it.
+# this will hopefully print out the name of the platform and we can do stuff with it
+sys_name_thing = platform.uname()
+# print(sys_name_thing.system)
+if sys_name_thing.system == "Windows":
+    print("You are on Windows!")
