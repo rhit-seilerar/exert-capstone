94f1e9ad34487b08174ee4ebf02f826be47d5c96
Evan Bestic <bestice@rose-hulman.edu>
2024-10-21 08:44:52 -0400

    Fixed mistaken deletion of files

---
+++ attach_to_container.bat
@@ -0,0 +1,2 @@
+REM Launch a new cmd process and attach it to the container
+START cmd /K docker exec -it pandare bash
---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Install some useful programs on the container
+apt-get update && apt-get install -y vim gdb cpio
+
+# Install some pip dependencies
+pip install ipython pytest pytest-mock
+
+# Symlink the qcows to the host so we only download them once
+if [[ ! -d /mount/.panda ]]; then mkdir /mount/.panda; fi
+if [[ -L /root/.panda ]]; then rm /root/.panda; fi
+ln -sf /mount/.panda /root/.panda
+
+# Move into the repo
+cd /mount
