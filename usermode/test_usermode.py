cbbec25b82b28e0ca8403c55566c3317efc22e2d
seilerar <seilerar@rose-hulman.edu>
2024-10-22 00:30:39 -0400

    Yml syntax error

--- usermode/test_usermode.py
+++ usermode/test_usermode.py
@@ -8,7 +8,8 @@ def _test_compile_i386():
         "make all ARCH=arm LIBC=musleabi",
         "popd"])
     assert os.path.isfile("usermode/build/helloworld")
-    assert utilities.get_stdout(utilities.run_command("usermode/build/helloworld", True)) == "hello world\n"
-    assert utilities.get_stderr(utilities.run_command("ldd usermode/build/helloworld", True)).strip() \
-        == "not a dynamic executable"
+    assert utilities.get_stdout(utilities.run_command("usermode/build/helloworld", True)) \
+        == "hello world\n"
+    assert utilities.get_stderr(utilities.run_command("ldd usermode/build/helloworld", True)) \
+        .strip() == "not a dynamic executable"
     print("Task successful!")
