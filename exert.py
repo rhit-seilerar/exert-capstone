61fe51e8848085ac6157db51327a7b88ba19a192
seilerar <seilerar@rose-hulman.edu>
2024-10-23 22:16:38 -0400

    Line too long

--- exert.py
+++ exert.py
@@ -78,7 +78,8 @@ def run_docker(container, name = None, command = '', persist = False):
     else:
         if not container_is_running(name):
             run_command(f'docker run --rm -dit --name {name} {mount} {container}')
-            run_command(f'docker exec -it {name} bash -c "cd /mount; chmod +x ./setup.sh; ./setup.sh"')
+            run_command(f'docker exec -it {name} bash -c '
+                f'"cd /mount; chmod +x ./setup.sh; ./setup.sh"')
         run_command(f'docker exec -it {name} bash -c "cd /mount; {command}"', False, False)
         if persist:
             run_command(f'docker exec -it {name} bash"')
