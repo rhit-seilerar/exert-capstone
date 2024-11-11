b1bd87bde9fc9c89845791d91cc951d91a265295
seilerar <seilerar@rose-hulman.edu>
2024-11-10 20:19:19 -0500

    Read tasklist from the stack!

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,40 +1,62 @@
+import subprocess
 from exert.usermode import plugin
 
 # Kernel info taken from https://panda.re/kernelinfos/ubuntu:4.4.0-170-generic:32.conf
 
-CALLBACK_WAS_CALLED = False
-def test_ground_truth_tasklist():
-    # pylint: disable=unused-argument
-    global CALLBACK_WAS_CALLED
-    CALLBACK_WAS_CALLED = False
-    def callback(panda, cpu):
-        global CALLBACK_WAS_CALLED
-        CALLBACK_WAS_CALLED = True
-        init_addr = 0xc1b1da80
-        parent_offset = 804
-        mem = panda.virtual_memory_read(cpu, init_addr + parent_offset, 4)
-        parent_addr = int.from_bytes(mem, 'little')
-        print(f'{parent_addr:02X}')
-        assert parent_addr == init_addr
-    plugin.run(callback = callback)
-    assert CALLBACK_WAS_CALLED
+def read_mem(panda, cpu, addr, size):
+    return panda.virtual_memory_read(cpu, addr, size)
 
-def _test_get_current_tasklist():
-    # pylint: disable=unused-argument
+def read_word(mem, offset):
+    return int.from_bytes(mem[offset:offset+4], byteorder='little', signed=False)
+
+CALLED_BACK = False
+def set_called_back(called_back):
+    global CALLED_BACK
+    CALLED_BACK = called_back
+
+TEST_PREFIX = """
+import tests.test_plugin
+tests.test_plugin.run_test('{}', tests.test_plugin.{})
+"""
+def do_test(arch, test):
+    print(TEST_PREFIX.format(arch, test.__name__))
+    subprocess.run(['python'], input = TEST_PREFIX.format(arch, test.__name__),
+        check = True, text = True)
+
+def run_test(arch, test):
+    set_called_back(False)
     def callback(panda, cpu):
-        # We're going to keep using the ground-truth offset for now
-        parent_offset = 804
-        # In linux kernel 2.6.26 and up, per-cpu info (such as the current task) was moved to
-        # a static pointer stored in the .data or .data..percpu section.
-        # We can stil test earlier versions though. Previously, a the current task pointer was
-        # stored at the base of the stack, e.g. masking off either 12 or 13 bits.
-        stack_pointer_name = list(panda.arch.registers.keys())[panda.arch.reg_sp]
-        stack_pointer = panda.arch.get_reg(cpu, stack_pointer_name)
-        current_task_4k = stack_pointer & 0xfffff000
-        current_task_8k = stack_pointer & 0xffffe000
-        mem_8k = panda.virtual_memory_read(cpu, current_task_8k + parent_offset, 4)
-        mem_4k = panda.virtual_memory_read(cpu, current_task_4k + parent_offset, 4)
-        parent_addr_4k = int.from_bytes(mem_4k, 'little')
-        parent_addr_8k = int.from_bytes(mem_8k, 'little')
-        assert parent_addr_4k == current_task_4k or parent_addr_8k == current_task_8k
-    plugin.run(callback = callback)
+        set_called_back(True)
+        test(panda, cpu)
+    plugin.run(arch = arch, callback = callback)
+    assert CALLED_BACK
+
+def callback_test_ground_truth_tasklist(panda, cpu):
+    init_addr = 0xc1b1da80
+    parent_offset = 804
+
+    init_task = read_mem(panda, cpu, init_addr, parent_offset + 4)
+
+    parent_addr = read_word(init_task, parent_offset)
+    assert parent_addr == init_addr
+def test_ground_truth_tasklist():
+    do_test('i386', callback_test_ground_truth_tasklist)
+
+def callback_test_get_current_from_stack(panda, cpu):
+    sp = panda.arch.get_reg(cpu, 'SP')
+
+    thread_info_addr = sp & ~(8192 - 1)
+    thread_info = read_mem(panda, cpu, thread_info_addr, 80)
+
+    task_addr = read_word(thread_info, 12)
+    task = read_mem(panda, cpu, task_addr, 400)
+
+    task_stack = read_word(task, 4)
+    assert task_stack == thread_info_addr
+def test_get_current_from_stack():
+    do_test('arm', callback_test_get_current_from_stack)
+
+def callback_test_get_current_from_segs(panda, cpu):
+    pass
+def test_get_current_from_segs():
+    do_test('i386', callback_test_get_current_from_segs)
