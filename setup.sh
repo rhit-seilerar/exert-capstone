87a505503fd4b8feb75a535b3291ddb1b0bd7c21
seilerar <seilerar@rose-hulman.edu>
2024-11-12 16:14:54 -0500

    Fix the busybox dir

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ apt-get install -y cpio
 mkdir cache
 cd cache
 mkdir busybox
+cd busybox
 wget https://www.busybox.net/downloads/binaries/1.21.1/busybox-binaries.tar.bz2
 tar -xf busybox-binaries.tar.bz2
 cp busybox-i486 busybox-i386
 rm busybox-binaries.tar.bz2
-
+cd ..
 
 # Install some pip dependencies
 python3 -m pip install --upgrade pip
