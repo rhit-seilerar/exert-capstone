e716033ea580420c86057abf1b6bbf203a9fef89
Evan Bestic <bestice@rose-hulman.edu>
2025-03-23 18:58:50 -0400

    Fixed linter coverage for both plugin.py and version.py.

--- exert/usermode/plugin.py
+++ exert/usermode/plugin.py
@@ -76,10 +76,9 @@ def run(arch = 'i386', callback = None, generic = True, kernel = None,
         arch_type = arch
         mem_use = "256M"
         hardware_args = ''
-        console_version = "ttyAMA0"
+        console_ver = "ttyAMA0"
         expect_prompt_entry = '/.*#'
-        os_version_entry = 'linux-32-generic'
-        
+        os_ver_entry = 'linux-32-generic'
         if (arch in ['armv4l', 'armv5l', 'armv6l', 'armv7l']):
             hardware_args = '-machine versatilepb'
             arch_type = 'arm'
@@ -87,18 +86,18 @@ def run(arch = 'i386', callback = None, generic = True, kernel = None,
             hardware_args = "-machine virt -cpu cortex-a53"
             expect_prompt_entry = '~ # '
             arch_type = 'aarch64'
-            os_version_entry = 'linux-64-generic'
+            os_ver_entry = 'linux-64-generic'
         else:
-            console_version = 'ttyS0'
+            console_ver = 'ttyS0'
 
         args = f'--nographic \
             -kernel {kernel} \
             -initrd ./cache/customfs.cpio \
             {hardware_args} \
-            -append "console={console_version} earlyprintk=serial nokaslr init=/bin/sh root=/dev/ram0"'
+            -append "console={console_ver} earlyprintk=serial nokaslr init=/bin/sh root=/dev/ram0"'
         panda = Panda(
             arch=arch_type, mem=mem_use, extra_args=args,
-            expect_prompt=expect_prompt_entry, os_version=os_version_entry)
+            expect_prompt=expect_prompt_entry, os_version=os_ver_entry)
 
 
     panda.pyplugins.load(Exert, args={
@@ -121,13 +120,13 @@ def run(arch = 'i386', callback = None, generic = True, kernel = None,
 def get_task_address(kernel, arch, version):
     version_supported = False
 
-    version_entry = ver.version_from_string(version)
+    ver_entry = ver.version_from_string(version)
 
     print("LOCATING TASK ADDRESS")
-    min_version = ver.Version(2,6,13)
-    max_version = ver.Version(5,14,21)
+    min_ver = ver.Version(2,6,13)
+    max_ver = ver.Version(5,14,21)
     if (arch in ['armv4l', 'armv5l', 'armv6l', 'armv7l']):
-        if(ver.compare_version(version_entry, min_version) and ver.compare_version_max(version_entry, max_version):
+        if(ver.compare_version(ver_entry, min_ver) and ver.compare_version(max_ver, ver_entry)):
             version_supported = True
             run(arch, task_struct_stack.task_address_arm_callback, False, kernel)
             print("Task Address: " + hex(task_struct_stack.TASK_ADDRESS))
--- exert/utilities/version.py
+++ exert/utilities/version.py
@@ -6,21 +6,18 @@ class Version:
     y: int
     z: int
 
-def version_from_string(input):
+def version_from_string(entry):
     number_string = input.split('-')[0]
     nums = number_string.split('.')
     v = Version(int(nums[0]), int(nums[1]), int(nums[2]))
     return v
 
 def compare_version(entry, target):
-    if(entry.x > target.x):
+    if entry.x > target.x:
         return True
-    elif(entry.x == target.x):
-        if(entry.y > target.y):
+    if entry.x == target.x:
+        if entry.y > target.y:
             return True
-        elif(entry.y == target.y and entry.z >= target.z):
+        if entry.y == target.y and entry.z >= target.z:
             return True
     return False
-
-def compare_version_max(entry, target):
-    return compare_version(target, entry)
