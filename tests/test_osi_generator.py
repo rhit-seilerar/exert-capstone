95a2ddca73cd1b87d6c414bfd87befdc036f2016
Evan Bestic <bestice@rose-hulman.edu>
2025-04-06 18:31:47 -0400

    Additional test cases for test_osi_generator.py

--- tests/test_osi_generator.py
+++ tests/test_osi_generator.py
@@ -13,8 +13,26 @@ def test_osi_generator_aarch():
     subprocess.run(['python', '-u', '-m', 'exert.osi_generator', './kernels/vmlinuz-aarch64-4.4.100',
                     'aarch64', '4.4.100'], check = True)
     
-def test_osi_generator_unsupported():
+def test_osi_generator_i386():
+    if not RUN_PLUGIN_TESTS:
+        return
+    subprocess.run(['python', '-u', '-m', 'exert.osi_generator', './kernels/vmlinuz-i386-4.4.100',
+                    'i386', '4.4.100'], check = True)
+
+def test_osi_generator_unsupported_version():
     if not RUN_PLUGIN_TESTS:
         return
     subprocess.run(['python', '-u', '-m', 'exert.osi_generator', './kernels/vmlinuz-aarch64-4.4.100',
-                    'aarch64', '6.12.1'], check = True)
+                    'aarch64', '6.12.1'], check = True) 
+
+def test_osi_generator_unsupported_arch():
+    if not RUN_PLUGIN_TESTS:
+        return
+    subprocess.run(['python', '-u', '-m', 'exert.osi_generator', './kernels/vmlinuz-aarch64-4.4.100',
+                    'ia64', '6.12.1'], check = True)
+
+def test_osi_generator_nonexistent_kernel():
+    if not RUN_PLUGIN_TESTS:
+        return
+    subprocess.run(['python', '-u', '-m', 'exert.osi_generator', './kernels/vmlinuz-idontexist-4.5.1',
+                    'aarch64', '4.4.100'], check = True) 
