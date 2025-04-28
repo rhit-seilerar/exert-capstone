31cb6979a942fa50c8919c59761eac4c795586f9
rhit-frantzag <frantzag@rose-hulman.edu>
2025-04-28 11:44:44 -0400

    Get rid of circular import error

--- exert/utilities/types/global_types.py
+++ exert/utilities/types/global_types.py
@@ -1,8 +1,11 @@
-from exert.parser.defoption import DefOption # Even if the types are strings, the type parser still needs to be able to see them
-from exert.parser.expressions import Expression
+# from exert.parser.defoption import DefOption # Even if the types are strings, the type parser still needs to be able to see them
+# from exert.parser.expressions import Expression
 
-type TokenType = (tuple[str, str | int] | tuple[str, str | int, str | set['DefOption']])
+# type TokenType = (tuple[str, str | int] | tuple[str, str | int, str | set['DefOption']])
+type TokenType = (tuple[str, str | int] | tuple[str, str | int, str | set])
 
-type TokenType3 = tuple[str, str | int, str | set['DefOption']]
+# type TokenType3 = tuple[str, str | int, str | set['DefOption']]
+type TokenType3 = tuple[str, str | int, str | set]
 
-type ExpressionTypes = 'Expression' | str | int
+# type ExpressionTypes = 'Expression' | str | int
+type ExpressionTypes = str | int
